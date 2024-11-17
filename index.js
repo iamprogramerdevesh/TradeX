2dc1e80b97f2c24afd74f09c0d6129f456fe3fac
iamdeveloperdevesh <154750450+iamprogramerdevesh@users.noreply.github.com>
2024-11-17 13:07:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,13 @@ import corsOptions from "./src/config/corsOptions.js";
 import credentials from "./src/config/corsCredentials.js";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 //#region CONFIGURATIONS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 6000;
@@ -29,9 +34,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.json());
 app.use(cookieParser());
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
 //#endregion
 
 //#region App Routes
@@ -42,6 +44,13 @@ app.use("/api", userRoutes);
 app.use("/api", accountRoutes);
 app.use("/api", tagRoutes);
 app.use("/api", tradeRoutes);
+//#endregion
+
+//#region Static files
+app.use(express.static(path.join(__dirname, './client/build')));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'))
+});
 //#endregion
 
 //#region MONGOOSE SETUP
